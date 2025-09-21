b39f0492dd62301f2fa7ac1bee8b78a1f2272020
rashed <bangla.bd.rashed@gmail.com>
2025-09-21 09:18:40 +0600

    my portfolio app source code

---
+++ routes/api.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+Route::post('register', [AuthController::class, 'register'])->name('user.register');
+Route::post('login', [AuthController::class, 'login'])->name('user.login');
+Route::delete('logout', [AuthController::class, 'logout'])->name('user.logout')->middleware('auth:sanctum');
+
+
